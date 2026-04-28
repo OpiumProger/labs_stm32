eb9b1b1d9ad37c70a92eddc06f84e80c5f865491
Krispis2005 <stepanovaks05@mail.ru>
2026-04-28 12:19:37 +0300

    lab3-fixed

--- Core/Src/platform/lab3_stepper/lab3_stepper.c
+++ Core/Src/platform/lab3_stepper/lab3_stepper.c
@@ -6,15 +6,15 @@
  * необходимый для работы с периферией МК stm32 */
 #include "main.h"
 
-#define STP1_GPIO_Port	GPIOA
-#define STP2_GPIO_Port	GPIOA
-#define STP3_GPIO_Port	GPIOA
-#define STP4_GPIO_Port	GPIOA
+#define STP1_GPIO_Port	SM1_GPIO_Port
+#define STP2_GPIO_Port	SM2_GPIO_Port
+#define STP3_GPIO_Port	SM3_GPIO_Port
+#define STP4_GPIO_Port	SM4_GPIO_Port
 
-#define STP1_Pin	GPIO_PIN_0
-#define STP2_Pin	GPIO_PIN_0
-#define STP3_Pin	GPIO_PIN_0
-#define STP4_Pin	GPIO_PIN_0
+#define STP1_Pin	SM1_Pin
+#define STP2_Pin	SM2_Pin
+#define STP3_Pin	SM3_Pin
+#define STP4_Pin	SM4_Pin
 
 /**
  * @brief выполняет переключение обмоток в рамках одного шага ШД
@@ -74,26 +74,104 @@ void plt_stepper_full(uint8_t step_n)
  */
 void plt_stepper_half(uint8_t half_step_n)
 {
-
+	switch (half_step_n) {
+			case 0:
+				HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+				break;
+	        case 1:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	        case 2:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+	            break;
+	        case 3:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+	            break;
+	        case 4:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+	            break;
+	        case 5:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	        case 6:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	        case 7:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	        case 8:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	        default:
+	            HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+	            HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+	            break;
+	}
 }
 
 /**
- * @brief вращает шаговый двигатель в заданном направлении
+ * @brief выполняет переключение обмоток в рамках одного полу-шага ШД
  *
- * @param dir - направление вращения, <-1> - против часовой стрелки, <1> - по часовой стрелке
- * <0> - двигатель остановлен
+ * @param half_step_n - номер полу-шага
  * @return return
  */
+
 void plt_stepper(int dir)
 {
-	/* */
-//	static int step_n = 0;
-//	switch (step_n) {
-//		case value:
-//
-//			break;
-//		default:
-//			break;
-//	}
+    static int step_n = 0;
+    static int step_mode = 0;
+    static int max_step = 4;
 
+    if (step_mode == 0) {
+        max_step = 4;
+    } else {
+        max_step = 8;
+    }
+
+    if (dir > 0) {
+        step_n++;
+        if (step_n >= max_step) {
+            step_n = 0;
+        }
+    } else if (dir < 0) {
+        step_n--;
+        if (step_n < 0) {
+            step_n = max_step - 1;
+        }
+    }
+
+    if (step_mode == 0) {
+        plt_stepper_full(step_n);
+    } else {
+        plt_stepper_half(step_n);
+    }
 }
--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -1,27 +1,36 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "platform.h"
 
-//#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
+#include "lab1_gpio/lab1_gpio.h"
+#include "lab2_timer/lab2_timer.h"
+#include "lab3_stepper/lab3_stepper.h"
 
 
 /* Однократный вызов */
 int plt_init(void)
 {
-
-	return 0;
+    return 0;
 }
 
+int direction = 1;
+int step_counter = 0;
+int steps_per_rotation = 8;
 
 /* Повторяющийся вызов */
 void plt_process(void)
 {
 
-	/* Устанавливаем задержку */
-	plt_delay(500);
-}
+    plt_stepper(direction);
 
+    step_counter++;
+    plt_delay(15);
+
+    if (step_counter >= steps_per_rotation) {
+       direction = -direction;
+        step_counter = 0;
+
+    }
+}
 /* Перенести в main.c в user code */
 //void plt_delay(uint32_t delay_ms)
 //{
