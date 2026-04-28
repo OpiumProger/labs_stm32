95d38e3cf331d96b75577b6b54e38469d33b4f14
IceInVienna <svistak.vova.gpt@gmail.com>
2026-04-28 11:23:07 +0300

    lab3_chanched

--- Core/Src/platform/lab3_stepper/lab3_stepper.c
+++ Core/Src/platform/lab3_stepper/lab3_stepper.c
@@ -1,9 +1,7 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "lab3_stepper.h"
 
-/* Подключение заголовочного файла main.h
- * main.h в свою очередь подключает _hal.h" - файл
- * необходимый для работы с периферией МК stm32 */
+
 #include "main.h"
 
 #define STP1_GPIO_Port	GPIOA
@@ -74,7 +72,62 @@ void plt_stepper_full(uint8_t step_n)
  */
 void plt_stepper_half(uint8_t half_step_n)
 {
-
+  switch (half_step_n) {
+    case 0:  // Такт 1
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+    case 1:  // Такт 2
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+    case 2:  // Такт 3
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+    case 3:  // Такт 4
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+    case 4:  // Такт 5
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+    case 5:  // Такт 6
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+      break;
+    case 6:  // Такт 7
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+      break;
+    case 7:  // Такт 8
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_SET);
+      break;
+    default:
+      HAL_GPIO_WritePin(STP1_GPIO_Port, STP1_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP2_GPIO_Port, STP2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP3_GPIO_Port, STP3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(STP4_GPIO_Port, STP4_Pin, GPIO_PIN_RESET);
+      break;
+  }
 }
 
 /**
@@ -86,14 +139,21 @@ void plt_stepper_half(uint8_t half_step_n)
  */
 void plt_stepper(int dir)
 {
-	/* */
-//	static int step_n = 0;
-//	switch (step_n) {
-//		case value:
-//
-//			break;
-//		default:
-//			break;
-//	}
+    int half_step_n = 0;
 
+    if (dir == 0) {
+        return;
+    }
+
+    half_step_n += dir;
+
+    if (half_step_n >= 8) {
+        half_step_n = 0;  // сброс на 0
+    } else if (half_step_n < 0) {
+        half_step_n = 7;  // перескок на 7
+    }
+
+    plt_stepper_half(half_step_n);
+    plt_delay(15);
 }
+
--- Core/Src/platform/platform.c
+++ Core/Src/platform/platform.c
@@ -1,9 +1,9 @@
 /* Директива препроцессора для подключения заголовочного файла */
 #include "platform.h"
 #include "main.h"
-//#include "lab1_gpio/lab1_gpio.h"
-//#include "lab2_timer/lab2_timer.h"
-//#include "lab3_stepper/lab3_stepper.h"
+#include "lab1_gpio/lab1_gpio.h"
+#include "lab2_timer/lab2_timer.h"
+#include "lab3_stepper/lab3_stepper.h"
 #include "lab4_adc/lab4_adc.h"
 extern UART_HandleTypeDef huart1;
 /* Однократный вызов */
@@ -17,12 +17,21 @@ int plt_init(void)
 /* Повторяющийся вызов */
 void plt_process(void)
 {
-	/* Устанавливаем задержку */
-	plt_delay(500);
+    int direction = 1;
+    uint32_t last_step_time = 0;
+    int step_count = 0;
+
+    uint32_t current_time = HAL_GetTick();
+
+    if (current_time - last_step_time >= 50) {
+        plt_stepper(direction);
+        last_step_time = current_time;
+        step_count++;
+
+        if (step_count >= 200) {
+            direction = -direction;
+            step_count = 0;
+        }
+    }
 }
 
-/* Перенести в main.c в user code */
-//void plt_delay(uint32_t delay_ms)
-//{
-//	HAL_Delay(delay_ms);
-//}
