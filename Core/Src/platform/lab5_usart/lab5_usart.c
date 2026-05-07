b19c43e314321a298257073ab4cdc54a6e3e1730
IceInVienna <svistak.vova.gpt@gmail.com>
2026-05-07 18:47:18 +0300

    lab5 done

--- Core/Src/platform/lab5_usart/lab5_usart.c
+++ Core/Src/platform/lab5_usart/lab5_usart.c
@@ -8,29 +8,27 @@ extern UART_HandleTypeDef huart1;
 
 static char tx_buf[256];
 static char rx_byte = 0;
-volatile int uart_rx_flag = 0;
 
-static void send_response(uint8_t code, const char* msg)
-{
-    plt_uart_print("[CODE: %d] %s\r\n", code, msg);
-}
-
+volatile int uart_flag = 0;
+/* init UART и стартового меню */
 void plt_uart_init(void)
 {
-    plt_uart_send("\r\n=== LED CONTROL ===\r\n");
+    plt_uart_send("\r\nLED CONTROL\r\n");
     plt_uart_send("Enter 1 -> LED ON\r\n");
     plt_uart_send("Enter 0 -> LED OFF\r\n");
-    plt_uart_send("==================\r\n");
+    plt_uart_send("\r\n");
     plt_uart_send("Waiting input: ");
 
-    HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+    HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1); /* перезапуск и приём некст байта*/
 }
 
+/* send строки по UART */
 void plt_uart_send(const char *data)
 {
     HAL_UART_Transmit(&huart1, (uint8_t*)data, strlen(data), HAL_MAX_DELAY);
 }
 
+/* отформатированный принт */
 void plt_uart_print(const char *format, ...)
 {
     va_list args;
@@ -42,39 +40,76 @@ void plt_uart_print(const char *format, ...)
 
 int plt_uart_is_available(void)
 {
-    return uart_rx_flag;
+	return uart_flag;
 }
+
+static void send_response(uint8_t code, const char* msg)
+{
+    plt_uart_send("\r\n[CODE: ");
+
+    if (code == 0)
+    {
+        plt_uart_send("0] ");
+    }
+    else
+    {
+        plt_uart_send("1] ");
+    }
+
+    plt_uart_send(msg);
+    plt_uart_send("\r\nWaiting input: ");
+}
+
 
 void plt_uart_proccess(void)
 {
+	if(plt_uart_is_available())
+	{
+		uart_flag = 0;
+
+			    plt_uart_send(&rx_byte); /* отправка нажатого символа обратно */
+
+			    if (rx_byte == '1') /* если 1, то вкл */
+			    {
+			        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
+			        send_response(0, "LED ON");
+			    }
+			    else if (rx_byte == '0') /* если 0, то выкл */
+			    {
+			        HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
+			        send_response(0, "LED OFF");
+			    }
+			    else
+			    {
+			        if (rx_byte != '\r' && rx_byte != '\n')
+			        {
+			        	send_response(1, "Use only 1 or 0");
+			        }
+	    }
+
+	    HAL_UART_Receive_IT(&huart1, (uint8_t*)&rx_byte, 1);
+	}
     if(plt_uart_is_available())
     {
-        uart_rx_flag = 0;
+    	uart_flag = 0;
 
         plt_uart_send(&rx_byte);
 
         if (rx_byte == '1')
         {
             HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
-            send_response(RESPONSE_SUCCESS, "LED ON");
-            plt_uart_send("Waiting input: ");
+            send_response(0, "LED ON");
         }
         else if (rx_byte == '0')
         {
             HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_RESET);
-            send_response(RESPONSE_SUCCESS, "LED OFF");
-            plt_uart_send("Waiting input: ");
+            send_response(0, "LED OFF");
         }
         else
         {
             if (rx_byte != '\r' && rx_byte != '\n')
             {
-                send_response(RESPONSE_ERROR, "Use only 1 or 0");
-                plt_uart_send("Waiting input: ");
-            }
-            else
-            {
-                plt_uart_send("Waiting input: ");
+                send_response(1, "Use only 1 or 0");
             }
         }
 
@@ -82,9 +117,11 @@ void plt_uart_proccess(void)
     }
 }
 
+/* колбэк при получении байта */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     if (huart->Instance != USART1) return;
 
-    uart_rx_flag = 1;
+    uart_flag = 1;
 }
+
